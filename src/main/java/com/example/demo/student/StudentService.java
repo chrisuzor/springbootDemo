9a6b6dcb9fe92b40610e38e8f34845d388a65838
Christopher <christopher@ADMINISTRATORs-MacBook-Pro-2.local>
2022-06-06 03:44:03 +0100

    Demo code push

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Chris",
+                        28,
+                        LocalDate.of(1993, Month.SEPTEMBER, 10),
+                        "c.uzor@yahoo.com"
+
+                )
+        );
+    }
+}
